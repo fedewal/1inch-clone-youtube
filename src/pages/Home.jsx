0b858b34ba23a1bcaec845b874a474ca9c231ac7
fedewal <fedewaldeck@gmail.com>
2023-10-27 08:40:47 -0300

    cambios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Navbar } from 'react-bootstrap'
+import Footer from '../components/Footer'
+import Section1 from '../containers/Section1'
+import Section2 from '../containers/Section2'
+import Section3 from '../containers/Section3'
+import Section4 from '../containers/Section4'
+import Section5 from '../containers/Section5'
+import Section6 from '../containers/Section6'
+import Section7 from '../containers/Section7'
+import Section8 from '../containers/Section8'
+import Section9 from '../containers/Section9'
+import Section10 from '../containers/Section10'
+import Section11 from '../containers/Section11'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      {/*NAVBAR*/}
+      <Navbar/>
+      {/*SECTIONS*/}
+      <Section1/>
+      <Section2/>
+      <Section3/> 
+      <Section4/>
+      <Section5/>
+      <Section6/>
+      <Section7/>
+      <Section8/>
+      <Section9/>
+      <Section10/>
+      <Section11/>
+      {/*FOOTER*/}
+      <Footer/>
+    </div>
   )
 }
 
