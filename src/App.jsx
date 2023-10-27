d29cc9eee714725bd2c079532fcb435030970ec6
fedewal <fedewaldeck@gmail.com>
2023-10-27 00:58:32 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import {Home} from './pages/Home'
-import {Swap} from './pages/Swap'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Swap from './pages/Swap';
 
 const App = () => {
   return (
